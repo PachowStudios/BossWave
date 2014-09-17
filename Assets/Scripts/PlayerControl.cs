2a589c407f0fd873b27784a1f8f8f87b0cbb8527
Ryan Shea <ryansworld10@gmail.com>
2014-09-16 20:27:01 -0700

    Finished physics rewrite and added animations
    Readded animations and finished rewriting physics.
    Fixed several problems with full speed running and animations.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,9 +5,11 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour
 {
 	public float gravity = -25f;
+	public float turningSpeed = 1f;
 	public float walkSpeed = 5f;
 	public float runSpeed = 8.5f;
 	public float runFullSpeed = 10f;
+	public float runFullTime = 1.5f;
 	public float groundDamping = 20f;
 	public float inAirDamping = 5f;
 	public float jumpHeight = 5f;
@@ -25,6 +27,9 @@ public class PlayerControl : MonoBehaviour
 	private bool jump;
 	private bool run;
 
+	private bool runFull = false;
+	private float runFullTimer = 0f;
+
 	void Awake()
 	{
 		anim = GetComponent<Animator>();
@@ -35,13 +40,26 @@ public class PlayerControl : MonoBehaviour
 	{
 		right = Input.GetButton("Right");
 		left = Input.GetButton("Left");
-		jump = Input.GetButton("Jump");
 		run = Input.GetButton("Run");
+		jump = Input.GetButton("Jump");
+
+		run = run && (right || left);
+
+		anim.SetBool("Walking", right || left);
+		anim.SetBool("Running", run);
+		
+		if (jump)
+		{
+			anim.SetTrigger("Jump");
+		}
 	}
 
 	void FixedUpdate()
 	{
 		velocity = controller.velocity;
+
+		anim.SetBool("Grounded", controller.isGrounded);
+		anim.SetBool("Falling", velocity.y < 0f);
 
 		if (controller.isGrounded)
 		{
@@ -54,12 +72,7 @@ public class PlayerControl : MonoBehaviour
 
 			if (transform.localScale.x < 0f)
 			{
-				transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-			}
-
-			if (controller.isGrounded)
-			{
-				anim.Play("Walking");
+				Flip();
 			}
 		}
 		else if (left)
@@ -68,36 +81,59 @@ public class PlayerControl : MonoBehaviour
 
 			if (transform.localScale.x > 0f)
 			{
-				transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-			}
-
-			if (controller.isGrounded)
-			{
-				anim.Play("Walking");
+				Flip();
 			}
 		}
 		else
 		{
 			normalizedHorizontalSpeed = 0;
-
-			if (controller.isGrounded)
-			{
-				anim.Play("Idle");
-			}
 		}
 
 		if (jump && controller.isGrounded)
 		{
 			velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
-			anim.Play("Jumping");
+		}
+
+		if (run)
+		{
+			runFullTimer += Time.deltaTime;
+
+			if (runFullTimer >= runFullTime)
+			{
+				runFull = true;
+				anim.SetBool("Running_Full", runFull);
+			}
+		}
+		else if (runFullTimer > 0f)
+		{
+			runFullTimer = 0f;
+			runFull = false;
+			anim.SetBool("Running_Full", runFull);
 		}
 
 		float smoothedMovementFactor = controller.isGrounded ? groundDamping : inAirDamping;
 
-		velocity.x = Mathf.Lerp(velocity.x, normalizedHorizontalSpeed * (run ? runSpeed : walkSpeed), Time.fixedDeltaTime * smoothedMovementFactor);
+		velocity.x = Mathf.Lerp(velocity.x, normalizedHorizontalSpeed * (run ? (runFull ? runFullSpeed : runSpeed) : walkSpeed), Time.fixedDeltaTime * smoothedMovementFactor);
 
 		velocity.y += gravity * Time.fixedDeltaTime;
 
 		controller.move(velocity * Time.fixedDeltaTime);
+	}
+
+	void Flip()
+	{
+		transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		
+		if (runFullTimer > 0f)
+		{
+			runFullTimer = 0f;
+			runFull = false;
+			anim.SetBool("Running_Full", runFull);
+		}
+
+		if (Mathf.Abs(velocity.x) > turningSpeed && controller.isGrounded)
+		{
+			anim.SetTrigger("Turn");
+		}
 	}
 }
