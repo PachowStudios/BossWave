1539f0332dc5a17ede09e29c1e0a52a69dd9d6d6
Ryan Shea <ryansworld10@gmail.com>
2014-09-19 10:00:42 -0700

    Added Follow AI
    Added follow enemy AI type.

---
+++ Assets/Scripts/AI Types/Follow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : Enemy
+{
+	public float followBuffer = 1.5f;
+
+	private Transform player;
+
+	new void Awake()
+	{
+		base.Awake();
+
+		player = GameObject.Find("Player").transform;
+	}
+
+	void FixedUpdate()
+	{
+		InitialUpdate();
+
+		anim.SetBool("Walking", right || left);
+
+		if (player.position.x > transform.position.x + followBuffer)
+		{
+			right = true;
+			left = !right;
+		}
+		else if (player.position.x < transform.position.x - followBuffer)
+		{
+			left = true;
+			right = !left;
+		}
+		else
+		{
+			right = left = false;
+		}
+
+		GetMovement();
+		ApplyMovement();
+	}
+}
