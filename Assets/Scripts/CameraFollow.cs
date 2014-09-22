042785987b8afb4e1255b7e93d475954f82cfce1
Ryan Shea <ryansworld10@gmail.com>
2014-09-22 01:58:07 -0700

    Added camera following for small aspect ratios
    The camera now follows the player on aspect ratios narrower than 16:9.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Vector2 margin = new Vector2(1f, 1f);
+	public Vector2 smooth = new Vector2(8f, 8f);
+	public Vector2 minPosition;
+	public Vector2 maxPosition;
+
+	private Transform player;
+
+	void Awake()
+	{
+		player = GameObject.Find("Player").transform;
+
+		minPosition.x += camera.orthographicSize * camera.aspect;
+		maxPosition.x -= camera.orthographicSize * camera.aspect;
+
+		minPosition.y += camera.orthographicSize;
+		maxPosition.y -= camera.orthographicSize;
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 targetPosition = transform.position;
+
+		if (Mathf.Abs(transform.position.x - player.position.x) > margin.x)
+		{
+			targetPosition.x = Mathf.Lerp(transform.position.x, player.position.x, smooth.x * Time.deltaTime);
+		}
+
+		if (Mathf.Abs(transform.position.y - player.position.y) > margin.y)
+		{
+			targetPosition.y = Mathf.Lerp(transform.position.y, player.position.y, smooth.y * Time.deltaTime);
+		}
+
+		targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+		targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+		transform.position = new Vector3(targetPosition.x, targetPosition.y, transform.position.z);
+	}
+}
