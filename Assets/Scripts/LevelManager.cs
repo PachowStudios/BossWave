486e29092b35b0be1ade31732894aaacd06295d2
Ryan Shea <ryansworld10@gmail.com>
2014-09-25 20:42:00 -0700

    Spread out enemy spawning
    There is now a 0.5 second delay between each individual enemy spawn
    during a wave.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,14 +6,14 @@ using System.Linq;
 public class LevelManager : MonoBehaviour 
 {
 	[System.Serializable]
-	public struct SpawnWave
+	public struct Wave
 	{
 		public float startTime;
 		public float amount;
 		public Enemy.Difficulty difficulty;
 	}
 
-	public List<SpawnWave> waves;
+	public List<Wave> waves;
 	public List<Enemy> enemies;
 
 	private int currentWave = 0;
@@ -32,28 +32,34 @@ public class LevelManager : MonoBehaviour
 
 		if (currentWave < waves.Count && waveTimer >= waves[currentWave].startTime)
 		{
-			List<Enemy> possibleEnemies = new List<Enemy>();
-
-			foreach (Enemy enemy in enemies)
-			{
-				if (enemy.difficulty == waves[currentWave].difficulty)
-				{
-					possibleEnemies.Add(enemy);
-				}
-			}
-
-			if (possibleEnemies.Count > 0 && spawners.Count > 0)
-			{
-				for (int i = 0; i < waves[currentWave].amount; i++)
-				{
-					int enemyToSpawn = Mathf.RoundToInt(Random.Range(0f, possibleEnemies.Count - 1));
-					int spawnerToUse = Mathf.RoundToInt(Random.Range(0f, spawners.Count - 1));
-
-					Instantiate(possibleEnemies[enemyToSpawn], spawners[spawnerToUse].transform.position, Quaternion.identity);
-				}
-			}
-
+			StartCoroutine(SpawnWave(currentWave));
 			currentWave++;
+		}
+	}
+
+	IEnumerator SpawnWave(int wave)
+	{
+		List<Enemy> possibleEnemies = new List<Enemy>();
+
+		foreach (Enemy enemy in enemies)
+		{
+			if (enemy.difficulty == waves[wave].difficulty)
+			{
+				possibleEnemies.Add(enemy);
+			}
+		}
+
+		if (possibleEnemies.Count > 0 && spawners.Count > 0)
+		{
+			for (int i = 0; i < waves[wave].amount; i++)
+			{
+				int enemyToSpawn = Mathf.RoundToInt(Random.Range(0f, possibleEnemies.Count - 1));
+				int spawnerToUse = Mathf.RoundToInt(Random.Range(0f, spawners.Count - 1));
+
+				Instantiate(possibleEnemies[enemyToSpawn], spawners[spawnerToUse].transform.position, Quaternion.identity);
+
+				yield return new WaitForSeconds(0.5f);
+			}
 		}
 	}
 }
