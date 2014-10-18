138a6a5e299940d38b9038858616a54ea60d7ff4
Ryan Shea <ryansworld10@gmail.com>
2014-10-17 22:43:46 -0700

    Added pause overlay screen
    Also added iTween visual editor.

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Pause : MonoBehaviour 
 {
+	public float pauseOverlayDuration = 0.3f;
+
 	private bool paused = false;
 	private AudioSource[] sounds;
+
+	private CanvasGroup pauseScreen;
+
+	void Awake()
+	{
+		pauseScreen = GetComponent<CanvasGroup>();
+	}
 
 	void Update()
 	{
@@ -15,13 +25,30 @@ public class Pause : MonoBehaviour
 			if (!paused)
 			{
 				paused = true;
+				iTween.ValueTo(gameObject, iTween.Hash("from", 0f, 
+													   "to", 1f, 
+													   "time", pauseOverlayDuration,
+ 													   "easetype", iTween.EaseType.easeOutQuint,
+													   "onupdate", "updateOverlayAlpha", 
+													   "ignoretimescale", true));
 				TimeWarpEffect.StartWarp(0f, sounds);
 			}
 			else
 			{
 				paused = false;
 				TimeWarpEffect.EndWarp(sounds);
+				iTween.ValueTo(gameObject, iTween.Hash("from", 1f,
+													   "to", 0f,
+													   "time", pauseOverlayDuration,
+													   "easetype", iTween.EaseType.easeOutQuint,
+													   "onupdate", "updateOverlayAlpha",
+													   "ignoretimescale", true));
 			}
 		}
+	}
+
+	private void updateOverlayAlpha(float newValue)
+	{
+		pauseScreen.alpha = newValue;
 	}
 }
