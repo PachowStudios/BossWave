b922cbb57ca5f8a48a5b059240f8238d9734f4d7
Ryan Shea <ryansworld10@gmail.com>
2014-11-10 02:04:14 -0800

    Lasers now use Vectrosity instead of line renderers

--- Assets/Scripts/Projectile Types/SmartLaser.cs
+++ Assets/Scripts/Projectile Types/SmartLaser.cs
@@ -2,20 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Vectrosity;
 
-[RequireComponent(typeof(LineRenderer))]
 public class SmartLaser : Projectile
 {
 	[Range(2, 32)]
 	public int subDivisionsPerTarget = 16;
+	public Material material;
+	public float width = 2f;
+	public string sortingLayer = "Player";
+	public int sortingOrder = 1;
 
 	private float cooldownTime;
 	private float cooldownTimer;
 
 	private List<Enemy> targetEnemies = new List<Enemy>();
 	private List<Vector3> targets = new List<Vector3>();
-
-	private LineRenderer lineRenderer;
+	private VectorLine vectorLine;
 
 	new void Awake()
 	{
@@ -24,13 +27,22 @@ public class SmartLaser : Projectile
 		cooldownTime = 1f / shotSpeed;
 		cooldownTimer = cooldownTime;
 
-		lineRenderer = GetComponent<LineRenderer>();
+		VectorLine.SetCanvasCamera(Camera.main);
+		VectorLine.canvas.planeDistance = 9;
+		VectorLine.canvas.sortingLayerName = sortingLayer;
+		VectorLine.canvas.sortingOrder = sortingOrder;
+
+		vectorLine = new VectorLine("Laser", new List<Vector3>(), material, width, LineType.Continuous, Joins.Weld);
+		vectorLine.textureScale = 1.0f;
 	}
 
 	void FixedUpdate()
 	{
 		GetTargets();
 		UpdatePath();
+
+		vectorLine.material = material;
+		vectorLine.Draw();
 
 		cooldownTimer += Time.deltaTime;
 
@@ -41,36 +53,46 @@ public class SmartLaser : Projectile
 		}
 	}
 
+	void OnDestroy()
+	{
+		VectorLine.Destroy(ref vectorLine);
+	}
+
 	private void GetTargets()
 	{
 		targetEnemies.Clear();
 		targets.Clear();
 
-		Vector3 origin = PlayerControl.instance.gun.firePoint.position + new Vector3(0f, 0f, -1f);
+		Vector3 origin = PlayerControl.instance.gun.firePoint.position;
+		transform.position = origin;
 
 		targets.Add(origin);
-		//targets.Add(PlayerControl.instance.gun.firePoint.TransformPoint(new Vector3(2f, 0f, 0f)) + new Vector3(0f, 0f, -1f));
 
 		foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
 		{
 			targetEnemies.Add(enemy.GetComponent<Enemy>());
-			targets.Add(enemy.transform.position + new Vector3(0f, 0f, -1f));
+			targets.Add(enemy.collider2D.bounds.center);
 		}
 
 		targets = targets.OrderBy(v => v.DistanceFrom(origin)).ToList<Vector3>();
+
+		if (targets.Count == 1)
+		{
+			targets.Add(PlayerControl.instance.gun.firePoint.TransformPoint(new Vector3(50f, 0f, 0f)));
+		}
 	}
 
 	private void UpdatePath()
 	{
+		vectorLine.points3.Clear();
+
 		Vector3[] targetsArray = targets.ToArray<Vector3>();
 		int totalSubdivisions = (targets.Count - 1) * subDivisionsPerTarget;
-
-		lineRenderer.SetVertexCount(totalSubdivisions);
 
 		for (int i = 0; i < totalSubdivisions; i++)
 		{
 			Vector3 currentPoint = iTween.PointOnPath(targetsArray, i / (float)totalSubdivisions);
-			lineRenderer.SetPosition(i, currentPoint);
+			vectorLine.points3.Add(currentPoint);
 		}
 	}
 
