e96f37865128630ecdd268a7891cdc4591a971f5
Ryan Shea <ryansworld10@gmail.com>
2015-02-17 13:33:44 -0800

    Added custom cursor

---
+++ Assets/Scripts/UI/HUD/CustomCursor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomCursor : MonoBehaviour
+{
+	private static CustomCursor instance;
+
+	public Texture2D texture;
+	public Vector2 hotpoint;
+	public CursorMode mode;
+
+	public static CustomCursor Instance
+	{
+		get { return instance; }
+	}
+
+	private void Awake()
+	{
+		instance = this;
+
+		Cursor.SetCursor(texture, hotpoint, mode);
+	}
+}
