e92a991efa5905cc072fc68ac1ed166bd17d1d42
Ryan Shea <ryansworld10@gmail.com>
2015-04-25 17:36:50 -0700

    Added floors to level 2
    Building cover now disappears when in front of the player.
    Tweaked some environment sprites.

---
+++ Assets/Scripts/Environment/TransparentForeground.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(BoxCollider2D))]
+public sealed class TransparentForeground : MonoBehaviour
+{
+	#region Fields
+	public float hiddenOpacity = 0f;
+	public float fadeTime = 0.5f;
+
+	private bool hidden = false;
+
+	private SpriteRenderer spriteRenderer;
+	#endregion
+
+	#region MonoBehaviour
+	private void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	private void Update()
+	{
+		bool overlapping = PlayerControl.Instance.collider2D.bounds.Intersects(collider2D.bounds);
+
+		if (overlapping && !hidden)
+			Hide();
+		else if (!overlapping && hidden)
+			Show();
+	}
+	#endregion
+
+	#region Internal Helper Methods
+	private void Show()
+	{
+		spriteRenderer.DOKill();
+		spriteRenderer.DOFade(1f, fadeTime);
+		hidden = false;
+	}
+
+	private void Hide()
+	{
+		spriteRenderer.DOKill();
+		spriteRenderer.DOFade(hiddenOpacity, fadeTime);
+		hidden = true;
+	}
+	#endregion
+}
