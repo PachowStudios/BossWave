62cdb0b2eb159be40786f7a083c0579ec74f3552
Ryan Shea <ryansworld10@gmail.com>
2015-05-24 15:28:45 -0700

    Added new rocket projectile
    Also added new sprite effect.

--- Assets/Scripts/Projectile Types/Modifiers/AcceleratingMod.cs
+++ Assets/Scripts/Projectile Types/Modifiers/AcceleratingMod.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public sealed class AcceleratingMod : ProjectileMod
 {
 	#region Fields
-	public float accelTime = 1f;
+	public float accelTime = 0f;
+	public float accelLength = 1f;
 	public AnimationCurve accelCurve;
 	public bool hasTrail = false;
-	[Range(0f, 1f)]
-	public float trailThreshold = 0f;
+	public float trailTime = 0f;
 
 	private float originalSpeed;
 	private float accelTimer = 0f;
@@ -17,13 +17,8 @@ public sealed class AcceleratingMod : ProjectileMod
 	#endregion
 
 	#region Internal Properties
-	private float accelPercentage
-	{
-		get
-		{
-			return accelCurve.Evaluate(Mathf.Clamp(accelTimer / accelTime, 0f, 1f));
-		}
-	}
+	private float AccelPercentage
+	{ get { return accelCurve.Evaluate(Mathf.Clamp((accelTimer - accelTime) / accelLength, 0f, 1f)); } }
 	#endregion
 
 	#region MonoBehaviour
@@ -33,22 +28,20 @@ public sealed class AcceleratingMod : ProjectileMod
 
 		anim = GetComponent<Animator>();
 		originalSpeed = thisProjectile.shotSpeed;
+		thisProjectile.shotSpeed = originalSpeed * accelCurve.Evaluate(0f);
 	}
 	#endregion
 
 	#region PublicMethods
 	public override void ApplyModifier()
 	{
-		if (accelPercentage < 1f)
-		{
-			accelTimer += Time.deltaTime;
-			thisProjectile.shotSpeed = originalSpeed * accelPercentage;
+		accelTimer += Time.deltaTime;
 
-			if (hasTrail && accelPercentage >= trailThreshold)
-			{
-				anim.SetBool("Trail", true);
-			}
-		}
+		if (accelTimer >= accelTime && AccelPercentage < 1f)
+			thisProjectile.shotSpeed = originalSpeed * AccelPercentage;
+
+		if (hasTrail && accelTimer >= trailTime)
+			anim.SetBool("Trail", true);
 	}
 	#endregion
 }
--- Assets/Scripts/Projectile Types/Modifiers/HomingMod.cs
+++ Assets/Scripts/Projectile Types/Modifiers/HomingMod.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class HomingMod : ProjectileMod
 {
 	#region Fields
-	public float homingStartTime = 0f;
+	public float homingTime = 0f;
 	public float homingLength = 0f;
 	[Range(0f, 1f)]
 	public float homingSpeed = 0.5f;
@@ -15,10 +15,10 @@ public class HomingMod : ProjectileMod
 	#region Public Methods
 	public override void ApplyModifier()
 	{
-		if (homingTimer <= homingStartTime + homingLength)
-		{
-			homingTimer += Time.deltaTime;
+		homingTimer += Time.deltaTime;
 
+		if (homingTimer >= homingTime && homingTimer <= homingTime + homingLength)
+		{
 			Vector2 targetDirection = transform.position.LookAt2D(PlayerControl.Instance.collider2D.bounds.center) * Vector3.right;
 			thisProjectile.direction = Vector3.Lerp(thisProjectile.direction, targetDirection, homingSpeed * 60f * Time.deltaTime).normalized;
 		}
